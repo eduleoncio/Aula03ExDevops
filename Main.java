749016f69f2de51eaecff6b77fbc68328eece77b
eduleoncio <eduardoleoncio1607@gmail.com>
2026-08-20 22:23:27 -0300

    Alteração na pipeline para teste de build

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("Olá, Devops!");
         System.out.println("Pipeline criada com sucesso!");
+        System.out.println("Alteração na pipeline para teste de build");
+
     }
 }
