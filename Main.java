dc461fe5c5994b3d579ceba9ef40afddfb445938
eduleoncio <eduardoleoncio1607@gmail.com>
2026-08-20 21:55:37 -0300

    Aula 03 Devops exemplo pipeline

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Olá, Devops!");
+        System.out.println("Pipeline criada com sucesso!");
+    }
+}
